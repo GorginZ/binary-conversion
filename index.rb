48d82e67a0e8a72be420d8473419908fb9f3dc64
GorginZ <georgia.z.leng@gmail.com>
2020-04-23 17:57:18 +1000

    array sorter and transposition

--- index.rb
+++ index.rb
@@ -10,12 +10,51 @@ x = 1
 while x <= num
 	array << x
 	puts 
-	x = x *2 
-	print array
-end 
+    x = x *2 
+
+    print array
+    new_array = array.reverse
+    puts
+    # print array.reverse
+    print new_array
+    
+end
+
+new_array.each do |a|
+    a = num - a
+    if a < 0
+        print "0"
+        a = num
+    # end
+elsif a >= 0
+        print "1"
+    end
+num = a
+end
+
+
+
+# rem = 1
+# while rem > 0, a++ do
+# rem == num - new_array[a]
+# if rem = 0
+# print "1"
+# end
+# elsif rem > 0
+# print "0"
+# end
+# end
+# print new_array
+
+
+        
 
 puts "Done!"
 
 #problem for tomorrow is getting a one
 
-puts 11.to_s(2)
+# result.reverse
+
+# return result.
+
+#new array where if that index value can be used to sum of num gets 1 else gets 0.
